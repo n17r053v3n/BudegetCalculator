ff033e0b9ba7266136e47fbb0fa76b697f643dd2
n17r053v3n <n17r053v3n@protonmail.com>
2022-01-28 15:52:30 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Navbar from "./Navbar.svelte";
+	import Title from "./Title.svelte";
+</script>
+
+<Navbar />
+<Title title="add expense"/>
+<Title title="expense list"/>
+<Title/>
+<div>
+
+</div>

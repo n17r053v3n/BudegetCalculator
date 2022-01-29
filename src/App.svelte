2c30fee123d10fed93bc994fe4bd53b30d485b81
n17r053v3n <n17r053v3n@protonmail.com>
2022-01-29 15:40:16 +0100

    Added functionality, still working

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,23 @@
 <script>
+	import { setContext } from "svelte";
+	// components
 	import Navbar from "./Navbar.svelte";
-	import Title from "./Title.svelte";
+	import ExpensesList from "./ExpensesList.svelte";
+	import Expense from "./Expense.svelte";
+
+	//	data
+	import expendesData from "./expenses";
+	// variables
+	let expenses = [...expendesData];
+	// functions
+	function removeExpense(id){
+		expenses = expenses.filter(item => item.id !== id);
+	}
+	// context
+	setContext('remove', removeExpense)
 </script>
 
 <Navbar />
-<Title title="add expense"/>
-<Title title="expense list"/>
-<Title/>
-<div>
-
-</div>
+<main class="content">
+	<ExpensesList {expenses}  />
+</main>
---
+++ src/ExpensesList.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Title from "./Title.svelte";
+    import Expense from "./Expense.svelte";
+    export let expenses = [];
+    
+</script>
+
+<section>
+    <Title title="Expense list"></Title>
+    <ul>
+        {#each expenses as expense, index}
+            <Expense {...expense} />
+            {:else}
+            <h2>Currently you have no expenses</h2>
+        {/each}
+    </ul>
+</section>
